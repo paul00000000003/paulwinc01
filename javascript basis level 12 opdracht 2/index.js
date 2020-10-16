a30d762da5bf26ef12e7acd293798fe33cb72760
paul00000000003 <59771148+paul00000000003@users.noreply.github.com>
2020-10-16 10:43:02 +0200

    Add files via upload

---
+++ javascript basis level 12 opdracht 2/index.js
@@ -0,0 +1,11 @@
+console.log("Hallo Winc acedemy studenten");
+
+let getal1 = 1230941;
+let getal2 = 1823794;
+// Dit is een grote som
+/*
+console.log("Het product van " + getal1.toString() + " en " + getal2.toString() + " = " + (getal1 * getal2).toString());
+getal1 = 637236;
+getal2 = 52;
+console.log("Het quotiënt van " + getal1.toString() + " en " + getal2.toString() + " = " + (getal1 / getal2).toString());
+*/
