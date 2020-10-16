706edeb5f742bed33d5483ead0998059aa09d46a
paul00000000003 <59771148+paul00000000003@users.noreply.github.com>
2020-10-16 11:28:20 +0200

    Add files via upload

---
+++ javascript basis level 12 opdracht 3/index.js
@@ -0,0 +1,9 @@
+const age = 17;
+const isFemale = true;
+const driverStatus = "bob";
+
+if (isFemale) { console.log("U bent een vrouw"); } else { console.log("U bent een man"); }
+
+if (age >= 18) { console.log("Je mag naar binnen"); } else { console.log("Ik mag je niet binnen laten") };
+
+if (driverStatus == "bob") { console.log("U mag een bob en mag rijden"); } else { console.log("U mag niet rijden."); }
