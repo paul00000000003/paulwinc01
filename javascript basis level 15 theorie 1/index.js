8bf20e796c21c0c30392de3dc97f17736690bf18
paul00000000003 <59771148+paul00000000003@users.noreply.github.com>
2020-10-20 08:09:37 +0200

    Add files via upload

---
+++ javascript basis level 15 theorie 1/index.js
@@ -0,0 +1,4 @@
+    var a = document.querySelector("#kop2");
+    a.innerHTML = "Dit kopje is met javascript gemaakt";
+    a = document.querySelector(".kop1");
+    a.innerHTML = "Dit is mijn eerste heading";
